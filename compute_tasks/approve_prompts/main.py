38f8a7b5aa148ae7968e6d8585ac244f2b9ab17b
Ioan Moldovan <ioan.moldovan@gmail.com>
2023-07-22 12:00:24 +0300

    Approve task, notebook changes

--- compute_tasks/approve_prompts/main.py
+++ compute_tasks/approve_prompts/main.py
@@ -11,12 +11,13 @@ from weaveapi.weaveh import *
 
 params = {
     "persona": "paris_guide",
-    "accept": [ 1, 3],
-    "reject": [ 2 ]
+    "accept": [ 1, 3]
 }
 
 LOCAL_CONFIG = "local.config"
 #LOCAL_CONFIG = None
+
+persona = params["persona"];
 
 scope = "vault"
 in_table = persona + '_proposals'
@@ -26,12 +27,25 @@ def main():
     nodeApi, session = connect_weave_api(LOCAL_CONFIG)
 
     order = {"id" : "DESC"}
-    filter = Filter(FilterOp.in("id", params.accept), None, None, None)
+    filter = None
     reply = nodeApi.read(session, scope, in_table, filter, READ_DEFAULT_NO_CHAIN).get()
-    print(reply)
+    #print(reply)
 
     count = len(reply["data"])
     print(count)
+
+    for r in reply["data"]:
+        if r["id"] in params["accept"]:
+            filter = Filter(FilterOp.opand(FilterOp.eq("source", r["pubkey"]), FilterOp.eq("text", r["text"])), None, None, None)
+            racc = nodeApi.read(session, scope, out_table, filter, READ_DEFAULT_NO_CHAIN).get()
+            exists = len(racc["data"]) > 0
+            if not exists:
+                #print(r)
+                records = Records(out_table, [
+                    [ None, None, None, None, None, "*", r["pubkey"], r["text"] ]
+                ])
+                res = nodeApi.write(session, scope, records, WRITE_DEFAULT).get()
+                print(res)
 
     #records = Records(out_table, [ ... ])
     #res = nodeApi.write(session, scope, records, WRITE_DEFAULT).get()
